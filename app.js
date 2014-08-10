c9aa750e3e8b15286d459d920941e18e8f08cad2
ibCom <support@ibcom.biz>
2014-08-10 15:03:01 +1000

    update for node-schedule

--- app.js
+++ app.js
@@ -4,5 +4,17 @@ mydigitalstructure.init('', mydigitalstructure.logon(appMain))
 
 function appMain(err, data)
 {
-	console.log(data)
+	//console.log(data)
+
+	var http = require('http');
+
+	http.createServer(function (req, res)
+	{
+  		res.writeHead(200, {'Content-Type': 'text/plain'});
+  		res.end('Hello World\n');
+	}).listen(1337, '127.0.0.1');
+	
+	console.log('Server running at http://127.0.0.1:1337/');
 }
+
+34 854
